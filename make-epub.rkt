f80989ee451bf0b1df9368f83329c7e9ea6e5fc0
Santiago Pelufo <santiagopelufo@gmail.com>
2015-12-26 13:32:33 -0300

    core

--- make-epub.rkt
+++ make-epub.rkt
@@ -9,6 +9,7 @@
 (define (file->lines f)
   (for/list ([l (in-lines (open-input-file f))]) l))
 
+
 (define lang "en")
 (define keep #f)
 (define author (getenv "USER"))
@@ -19,15 +20,13 @@
   (command-line
    #:once-each
    [("-o") o "output epub file" (set! output-file o)]
-   [("-k") k "keep all generated epub files" (set! keep #t)]
    [("-a") a "author" (set! author a)]
    [("-l") l "language" (set! lang l)]
    [("-s") s "spine file" (set! spine-files (file->lines s))]
+   [("-k") "keep all generated epub files" (set! keep #t)]
    #:args
    (title . files) (values title files)))
 
-
-(current-directory output-dir)
 
 (unless spine-files
   (set! spine-files (filter html-file? files)))
@@ -35,6 +34,7 @@
 (make-epub-files files spine-files title author lang)
 
 (when output-file
+  (when (file-exists? output-file) (delete-file output-file))
   (system (string-append "zip -X -Z store " output-file " mimetype"))
   (system (apply string-append "zip -X -r " output-file " META-INF content.opf nav.xhtml " files)))
 
